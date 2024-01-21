c88cec495944c83b3a7442c3e9e13ed48e949a90
Bhavesh0709 <bhavesh.21910572@viit.ac.in>
2024-01-21 12:27:22 +0530

    spacing fix

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,7 +3,6 @@ import { sequelize } from './database/database';
 import { logger } from './logger/logger';
 import app from './routes/backendApi';
 
-
 const mainApp = express();
 const port = 3309;
 
@@ -12,17 +11,17 @@ mainApp.use(express.json());
 mainApp.use(app);
 
 sequelize.authenticate()
-  .then(() => {
-    logger.info('Connection to the database has been established successfully.')
-  })
-  .catch(error => {
-    logger.error('Unable to connect to the database:', error.message);
-  });
+.then(() => {
+  logger.info('Connection to the database has been established successfully.')
+})
+.catch(error => {
+  logger.error('Unable to connect to the database:', error.message);
+});
 
 sequelize.sync().then(() => {
-    app.listen(port, () => {
-        logger.info(`=== Server is running at port - ${port}`);
-    })
+  app.listen(port, () => {
+      logger.info(`=== Server is running at port - ${port}`);
+  })
 }).catch((err) => {
     logger.error('=== Error while connecting - ', err);
 })
